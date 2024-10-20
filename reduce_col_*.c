fdff68e9174b1db2b633ac4a6c0b939e69d8b0a0
Madhusri182007 <madhusri182007@gmail.com>
2024-10-20 19:04:29 +0530

    Create reduce_col_*.c

---
+++ reduce_col_*.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int N,row,col,t;
+    scanf("%d",&N);
+    t=N;
+    for(row=0;row<N;row++)
+    {
+        for(col=0;col<N-row;col++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
