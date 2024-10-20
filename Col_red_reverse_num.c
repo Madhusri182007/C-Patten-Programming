81c06adcc03b158630dfb8eb88ca791889c00abe
Madhusri182007 <madhusri182007@gmail.com>
2024-10-20 19:09:59 +0530

    Create Col_red_reverse_num.c

---
+++ Col_red_reverse_num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int N,row,col,t;
+    scanf("%d",&N);
+    t=N;
+    for(row=0;row<N;row++)
+    {
+        t=N-row;   
+        for(col=0;col<N-row;col++)
+        {
+            printf("%d ",t);
+            t=t-1;
+        }
+        printf("\n");
+    }
+}
